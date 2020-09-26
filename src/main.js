4ff7caaf1192b4dfd4bde019133e31000e702d62
k-kuwahara <zensin0082@gmail.com>
2020-09-26 23:54:00 +0900

    feat: added auth logic and buttons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import '@riotjs/hot-reload'
-import {component} from 'riot'
-import App from './app.riot'
+import "@riotjs/hot-reload";
+import { component } from "riot";
+import Amplify from "aws-amplify";
 
-component(App)(document.getElementById('app'), {
-  title: 'Hello Riot.js!'
-})
+import App from "./app.riot";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+component(App)(document.getElementById("app"), {
+  title: "AWS Amplify Tutorial with Riot.js!",
+});
