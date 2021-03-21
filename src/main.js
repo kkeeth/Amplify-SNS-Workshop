fdd26a4fedee27affbd36dc628d09ce7e609a1b7
kkeeth <zensin0082@gmail.com>
2021-03-22 00:32:42 +0900

    fix: doesn't show images

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import "@riotjs/hot-reload";
-import { component, register } from "riot";
+import { component } from "riot";
 import Amplify from "aws-amplify";
-import { Router, Route } from '@riotjs/route';
 
 import App from "./app.riot";
 import awsConfig from "./aws-exports";
-
-register('router', Router);
-register('route', Route);
-
 Amplify.configure(awsConfig);
 
 component(App)(document.getElementById("app"));
